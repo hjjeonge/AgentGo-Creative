1d23a82a7e447d99b87c7bd3f05a31f0326fbe99
hnryu <hnryu@cloit.com>
2026-02-23 16:33:09 +0900

    feat: 로그인 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { DashboardPage } from "./pages/DashboardPage";
 import { EditorPage } from "./pages/EditorPage";
 import { TemplatePage } from "./pages/TemplatePage";
 import { DAMPage } from "./pages/DAMPage";
+import { LoginPage } from "./pages/LoginPage";
 import { Layout } from "./components/commons/Layout";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route element={<Layout />}>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/template" element={<TemplatePage />} />
