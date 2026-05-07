fcb394defc666f8a51e868782ff53f0d0130dcca
hjkim10 <hjkim10@itcen.com>
2026-05-07 11:19:57 +0900

    feat: eslint-plugin-import 설치 및 eslint config 파일 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,19 @@
 import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    plugins: {
+      import: importPlugin,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -17,11 +21,35 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     rules: {
-      'react-hooks/exhaustive-deps': 'off',
-      'react-hooks/set-state-in-effect': 'off',
-      'react-hooks/immutability': 'off',
+      'react-hooks/exhaustive-deps': 'error',
+      'react-hooks/set-state-in-effect': 'warn',
+      'react-hooks/immutability': 'error',
       'react-hooks/preserve-manual-memoization': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: '@/**', // @로 시작하는 경로를 internal로 인식
+              group: 'internal',
+            },
+          ],
+          distinctGroup: false,
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc', // 알파벳순 정렬
+            caseInsensitive: true, // 대소문자 구분 X
+          },
+        },
+      ],
     },
     languageOptions: {
       ecmaVersion: 2020,
